3dfb472f74db0ec6c43c409cbd4b0e4388e1e70b
Olek <roguezilla0@gmail.com>
2020-05-16 10:55:00 +0100

    collision detection is my passion /s

--- src/pt/iscte/dcti/poo/sokoban/starter/Ice.java
+++ src/pt/iscte/dcti/poo/sokoban/starter/Ice.java
@@ -14,12 +14,19 @@ public class Ice extends SokobanObject implements ActiveObject {
         MovableObject movableObject = (MovableObject)object;
         //objects are moved in the direction the player is moving
         Direction playerDir = Sokoban.getInstance().getPlayer().getDirection();
-        //fixes the player and the movable object being moved to the same spot after the player tries moving it from when its on ice due to a previous collision
+        /*
+        2 bugs are fixed here:
+        1. fixes the player and the movable object being moved to the same spot after the player tries moving it from when its on ice due to a previous collision
+        2. objects getting moved twice
+        */
         SokobanObject onIce = Sokoban.getInstance().selectObject(sokobanObject -> sokobanObject.isAt(this.getPosition()) && sokobanObject instanceof MovableObject);
         if (onIce != null) {
             ((MovableObject)onIce).move(playerDir);
+            if (movableObject instanceof Player) {
+                movableObject.move(playerDir);
+            }
+        } else {
+            movableObject.move(playerDir);
         }
-        //move the object only after fixing the problem above(if present) so we don't get weird collision bugs
-        movableObject.move(playerDir);
     }
 }
